1a29dd60660f76cc2311ee1889da1bd40a2fa32d
victoryang00 <victoryang00@ucsc.edu>
2023-10-14 12:57:19 -0700

    fix test compilation

--- HW1/part1/my_tests.py
+++ HW1/part1/my_tests.py
@@ -10,41 +10,37 @@ Tests = [
                """
     },
     {
-    "Name"    : "Yiwei 2",
-	"Outcome" : "SymbolTableException"
-	"Program" : """
-                x = 62 + 78;
-                {
-                    x = 1;
-                }
+        "Name"    : "Yiwei 2",
+        "Outcome" : "SymbolTableException",
+        "Program" : """
+                    x = 62 + 78;
+                    {
+                        x = 1;
+                    }
                     y = x;
-                {
-                    x = 3;
-                }
-               """
+                    {
+                        x = 3;
+                    }
+                   """
     },
     {
-    "Name"    : "Yiwei 3",
-	"Outcome" : "SymbolTableException"
-	"Program" : """
-                x = y;
-                {
+        "Name"    : "Yiwei 3",
+        "Outcome" : "SymbolTableException",
+        "Program" : """
+                    x = y;
                     y = 12;
-                }
-               """
+                   """
     },
     {
-    "Name"    : "Yiwei 4",
-    "Outcome" : "SymbolTableException"
-    "Program" : """
-                x = 2;
-                y = 3;
-                z = (x + y) * (x - y) / x ^ y;
-                print(z);
-                z = x + y * x - y / x ^ y;
-                print(z);
-               """
-    },
-    {
+        "Name"    : "Yiwei 4",
+        "Outcome" : "SymbolTableException",
+        "Program" : """
+                    x = 2;
+                    y = 3;
+                    z = (x + y) * (x - y) / x ^ y;
+                    print(z);
+                    z = x + y * x - y / x ^ y;
+                    print(z);
+                   """
     }
     ]
