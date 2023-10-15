fecbed2fe299a5c5a6263d41ecb415011ec3e317
victoryang00 <victoryang00@ucsc.edu>
2023-10-14 21:15:50 -0700

    finish part2

--- HW1/part2/skeleton.py
+++ HW1/part2/skeleton.py
@@ -1,16 +1,16 @@
 import ply.lex as lex
 import ply.yacc as yacc
-from typing import TypedDict, Unpack
 
 
 tokens = ["LPAREN", "RPAREN", "STAR", "QUES", "CH", "DOT", "UNION"]
-t_CH = r"[a-zA-Z]"
+t_CH = r"[a-zA-Z0-9]"
 t_UNION = r"\|"
 t_DOT = r"\."
 t_LPAREN = r"\("
 t_RPAREN = r"\)"
 t_STAR = r"\*"
 t_QUES = r"\?"
+t_ignore = ' '
 
 
 def p_re_one(p):
@@ -20,6 +20,7 @@ def p_re_one(p):
 
 def p_re_two(p):
     "re : concat UNION re"
+    
     p[0] = make_union(p[1], p[3])
 
 
@@ -28,14 +29,16 @@ def p_concat_one(p):
     concat : star
             | optional
     """
+    
     p[0] = p[1]
 
 
-def concat_two(p):
+def p_concat_two(p):
     """
     concat : star DOT concat
             | optional DOT concat
     """
+    
     p[0] = make_concat(p[1], p[3])
 
 
@@ -44,6 +47,7 @@ def p_base_one(p):
     star : paren
     optional : paren
     """
+    
     p[0] = p[1]
 
 
@@ -52,21 +56,24 @@ def p_base_two(p):
     star : star STAR
     optional : optional QUES
     """
+    
     if p[2] == "*":
         p[0] = make_star(p[1])
     else:
-        p[0] = make_union(get_leaf(""), p[1])
+        p[0] = make_union(Leaf(""), p[1])
 
 def p_paren_one(p):
     """
     paren : symbol
     """
+    
     p[0] = p[1]
 
 def p_paren_two(p):
     """
     paren : LPAREN re RPAREN
     """
+    
     p[0] = p[2]
 
 
@@ -74,11 +81,12 @@ def p_char(p):
     """
     symbol : CH
     """
-    p[0] = get_leaf(p[1])
+
+    p[0] = Leaf(p[1])
 
 
 def p_error(p):
-    print("Syntax error in input!")
+    print(p,"Syntax error in input!")
     exit(0)
 
 # Required in case of a lexing error
@@ -90,51 +98,49 @@ def t_error(t):
 lexer = lex.lex()
 
 
-class Leaf(TypedDict):
-    charactor: str
+class Leaf():
+    character: str
+    def __init__(self, char):
+        self.character = char
 
-
-class Op(TypedDict):
+class Op():
     lhs: str
     rhs: str
     op: str
-
-
-def get_leaf(**kwargs: Unpack[Leaf]) -> None:
-    for k in kwargs.items():
-        return Leaf(k)
-
-
-def get_op(**kwargs: Unpack[Op]) -> None:
-    for k, v1, v2 in kwargs.items():
-        return Op(k, v1, v2)
+    def __init__(self, left, right, op):
+        self.lhs = left
+        self.rhs = right
+        self.op = op
 
 
 def make_union(re1, re2) -> Op:
-    if re1 is None or re2 is None:
-        return None
+    if re1 is None:
+        return re2
+    elif re2 is None:
+        return re1
     if isinstance(re1, Leaf) and isinstance(re2, Leaf):
         if re1.char == "" and re2.char == "":
-            return get_op("", None, "UNION")
-    return get_op(re1, re2, "OR")
+            return re1
+    return Op(re1, re2, "UNION")
 
 
 def make_star(re1) -> Leaf:
     if re1 is None:  # empty
         return None
     if isinstance(re1, Leaf):  # epsilon
-        if re1.char == "":
+        if re1.character == "":
             return re1
-    return get_leaf(re1, None, "STAR")
+    return Op(re1, None, "STAR")
 
 
 def make_concat(re1, re2) -> Op:
     if re1 is None or re2 is None:
         return None
-    if isinstance(re1, Leaf) and isinstance(re2, Leaf):
-        if re1.char == "" and re2.char == "":
-            return get_op("", None, "CONCAT")
-    return get_op(re1, re2, "CONCAT")
+    if isinstance(re1, Leaf) and re1.character == "":
+        return re2
+    if isinstance(re2, Leaf) and re2.character == "":
+        return re1
+    return Op(re1, re2, "CONCAT")
 
 
 ## Nullable function starting here:
@@ -148,17 +154,17 @@ def make_concat(re1, re2) -> Op:
 # Implement this function
 def nullable(re) -> Leaf:
     if isinstance(re, Leaf):
-        if re is None:
+        if re.character =="":
             return re
         else:
             return None
     elif isinstance(re, Op):
         if re.op == "CONCAT":
             return make_concat(nullable(re.lhs), nullable(re.rhs))
-        elif re.op == "OR":
+        elif re.op == "UNION":
             return make_union(nullable(re.lhs), nullable(re.rhs))
         elif re.op == "STAR":
-            return get_leaf("")
+            return Leaf("")
 
 
 # derivative function starting here:
@@ -183,26 +189,27 @@ def derivative_re(char, re):
     if re is None:
         return None
     elif isinstance(re, Leaf):
-        if re.char == char:
+        if re.character == "":
+            return None
+        elif re.character == char:
             return Leaf("")
         else:
             return None
-    if isinstance(re, Op):
+    elif isinstance(re, Op):
         if re.op == "CONCAT":
             return make_union(
                 make_concat(derivative_re(char, re.lhs), re.rhs),
                 make_concat(nullable(re.lhs), derivative_re(char, re.rhs)),
             )
-        elif re.op == "OR":
+        elif re.op == "UNION":
             return make_union(derivative_re(char, re.lhs), derivative_re(char, re.rhs))
         elif re.op == "STAR":
-            return make_concat(derivative_re(char, re.lhs), re)
+            return make_concat(derivative_re(char, re.lhs), make_star(re.lhs))
 
 
 parser = yacc.yacc()
 
-
-# High-level function to match a string using regular experession
+# High-level function to match a string using regular expression
 # derivatives:
 def match_regex_ast(re, to_match):
     # create the derivative for each character of the string in sequence
@@ -214,7 +221,7 @@ def match_regex_ast(re, to_match):
     # the string matches if and only if the empty string is matched by the derivative RE
     my_re = nullable(re)
     if isinstance(my_re, Leaf):
-        if my_re.char == "":
+        if my_re.character == "":
             return True
     return False  # you will need to write something like: is_epsilon(nullable(re)) here
 
@@ -226,16 +233,16 @@ def match_regex(reg_ex, string):
 
 # Use this conditional to test your script locally
 if __name__ == "__main__":
-    d_re = parser.parse("(h.i)* | c.s.e*.2.1.1")
+    d_re = parser.parse("(h.i)* | c.s.e*.2.1.1", debug=True)
 
     # should pass
-    print(parse_re(d_re, "hi") == True)
-    print(parse_re(d_re, "hihi") == True)
-    print(parse_re(d_re, "cse211") == True)
-    print(parse_re(d_re, "cs211") == True)
-    print(parse_re(d_re, "cseee211") == True)
+    print(match_regex_ast(d_re, "hi") == True)
+    print(match_regex_ast(d_re, "hihi") == True)
+    print(match_regex_ast(d_re, "cse211") == True)
+    print(match_regex_ast(d_re, "cs211") == True)
+    print(match_regex_ast(d_re, "cseee211") == True)
 
-    # should fail
-    print(parse_re(d_re, "hhh") == False)
-    print(parse_re(d_re, "cseee21") == False)
-    print(parse_re(d_re, "211") == False)
+    # # should fail
+    print(match_regex_ast(d_re, "hhh") == False)
+    print(match_regex_ast(d_re, "cseee21") == False)
+    print(match_regex_ast(d_re, "211") == False)
