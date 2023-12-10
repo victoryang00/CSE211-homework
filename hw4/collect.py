7ec386423f875cfa22369aed5349fc1055cc8390
victoryang00 <yangyiwei2000@gmail.com>
2023-12-10 15:38:55 -0800

    update results

--- hw4/collect.py
+++ hw4/collect.py
@@ -15,46 +15,66 @@ plt.rcParams.update(
 # Creating a clustered bar chart for the provided data
 
 # Data for the clustered bar chart
-default_configs = ["No AOF", "everysec", "alwayson"]
-default_throughput = [87435.52, 71669.17, 12584.47]
+default_configs = ["M3Max Metal", "M3Max CPU", "Alderlake CPU", "4090 CUDA"]
+default_throughput = [3711.20, 473.08, 280.73, 12584.47]
+bert_performance = [72.1661376953125, 153.1397819519043, 135.153040885925, 62.1795]
+import matplotlib.pyplot as plt
+import numpy as np
 
-batching_configs = ["Batch 1", "Batch 3", "Batch 12", "Batch 24", "Batch 48"]
-batching_throughput = [19031.67, 43200.27, 48081.55, 50309.40, 52562.42]
-
-fsync_configs = ["delayed-fsync", "delayed-fsync fast-notify"]
-fsync_throughput = [39677.82, 64724.92]
-
-# Plotting
-fig, ax = plt.subplots(figsize=(12, 10))
-
-# Cluster positions
+# Assuming default_configs, default_throughput, bert_performance are defined
 default_positions = np.arange(len(default_configs))
-batching_positions = np.arange(len(batching_configs)) + len(default_configs) + 1 # Offset by 1 for spacing
-fsync_positions = np.arange(len(fsync_configs)) + len(default_configs) + len(batching_configs) + 2 # Further offset
-
-# Bar width
 bar_width = 0.8
 
-# Plotting the clusters
-ax.bar(default_positions, default_throughput, width=bar_width, label="Default Configs", color='#8ecfc9')
-ax.bar(batching_positions, batching_throughput, width=bar_width, label="Batching Configs", color='#ffbe7a')
-ax.bar(fsync_positions, fsync_throughput, width=bar_width, label="Delayed-fsync", color='#fa7f6f')
+# Creating the first plot for TVM GEMM
+fig1, ax1 = plt.subplots(figsize=(12, 10))
+ax1.bar(
+    default_positions,
+    default_throughput,
+    width=bar_width,
+    label="TVM GEMM",
+    color="#8ecfc9",
+)
 
-# Labels and ticks
-ax.set_ylabel('Throughput (req/sec)')
-# ax.set_title('Redis Configuration Throughput Comparison')
-ax.set_xticks(np.concatenate([default_positions, batching_positions, fsync_positions]))
-ax.set_xticklabels(default_configs + batching_configs + fsync_configs, rotation=45, ha="right")
+# Labels and ticks for the first plot
+ax1.set_ylabel("GFLOPS")
+ax1.set_xticks(default_positions)
+ax1.set_xticklabels(default_configs)
 
-# Adding data labels
-for pos, val in zip(np.concatenate([default_positions, batching_positions, fsync_positions]), 
-                    np.concatenate([default_throughput, batching_throughput, fsync_throughput])):
-    ax.text(pos, val, f'{val/1000:.0f}K', ha='center', va='bottom')
+# Data labels for the first plot
+for pos, val in zip(default_positions, default_throughput):
+    ax1.text(pos, val, f"{val}", ha="center", va="bottom")
 
-# Adding grid
-ax.grid(True)
+# Grid for the first plot
+ax1.grid(True)
+ax1.legend()
 
-plt.legend()
+# Creating the second plot for TVM Bert
+fig2, ax2 = plt.subplots(figsize=(12, 10))
+ax2.bar(
+    default_positions,
+    bert_performance,
+    width=bar_width,
+    label="TVM Bert",
+    color="#ffbe7a",
+)
+
+# Labels and ticks for the second plot
+ax2.set_ylabel("Latency (ms)")
+ax2.set_xticks(default_positions)
+ax2.set_xticklabels(default_configs)
+
+# Data labels for the second plot
+for pos, val in zip(default_positions, bert_performance):
+    ax2.text(pos, val, f"{val}", ha="center", va="bottom")
+
+# Grid for the second plot
+ax2.grid(True)
+ax2.legend()
+
+# Layout adjustments and saving the figures
 plt.tight_layout()
-plt.savefig("redis_cluster.pdf")
+fig1.savefig("tvm_gemm.pdf")
+fig2.savefig("tvm_bert.pdf")
+
+# Display the plots
 plt.show()
